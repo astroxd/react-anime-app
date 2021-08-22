1b9f4d8b227d2901ad16dd5a787c633a3ab2ba23
Andrea Sciortino <andreasciortino2004@gmail.com>
2021-08-22 02:50:13 +0200

    add navbar ui for larger screen

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 // import { useEffect, useState } from 'react'
 // import { useSelector } from 'react-redux'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // import { useLocation } from 'react-router-dom'
 // import Searchbar from './Searchbar'
+import { Container, Row, Col, Dropdown } from 'react-bootstrap'
+import logo from './../assets/images/logo.png'
 
-const Navbar = () => {
+const CustomNavbar = () => {
 	// const selector = useSelector((state) => state.user)
 
 	// const [state, setstate] = useState(true)
@@ -25,81 +27,92 @@ const Navbar = () => {
 	// }, [location, selector])
 
 	return (
-		<nav>cacca</nav>
-		// <nav className='navbar'>
-		// 	<div className='logo'>
-		// 		<div className='logo-text'>ANIME</div>
-		// 	</div>
-		// 	<div className='mid-content'>
-		// 		<Searchbar />
-
-		// 		<div className='user-stuff'>
-		// 			<Link to='/watchlist' className='user-link'>
-		// 				<svg
-		// 					xmlns='http://www.w3.org/2000/svg'
-		// 					className={`h-6 w-6 ${
-		// 						path === '/watchlist' ? 'user-link-selected' : ''
-		// 					}`}
-		// 					fill='none'
-		// 					viewBox='0 0 24 24'
-		// 					stroke='currentColor'
-		// 				>
-		// 					<path
-		// 						strokeLinecap='round'
-		// 						strokeLinejoin='round'
-		// 						strokeWidth={2}
-		// 						d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
-		// 					/>
-		// 					<path
-		// 						strokeLinecap='round'
-		// 						strokeLinejoin='round'
-		// 						strokeWidth={2}
-		// 						d='M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z'
-		// 					/>
-		// 				</svg>
-		// 			</Link>
-		// 			<Link to='/favorite' className='user-link'>
-		// 				<svg
-		// 					xmlns='http://www.w3.org/2000/svg'
-		// 					className={`h-6 w-6 ${
-		// 						path === '/favorite' ? 'user-link-selected' : ''
-		// 					}`}
-		// 					fill='none'
-		// 					viewBox='0 0 24 24'
-		// 					stroke='currentColor'
-		// 				>
-		// 					<path
-		// 						strokeLinecap='round'
-		// 						strokeLinejoin='round'
-		// 						strokeWidth={2}
-		// 						d='M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z'
-		// 					/>
-		// 				</svg>
-		// 			</Link>
-		// 			<Link to='/' className='user-link'>
-		// 				<svg
-		// 					xmlns='http://www.w3.org/2000/svg'
-		// 					className={`h-6 w-6 ${
-		// 						path === '/user' ? 'user-link-selected' : ''
-		// 					}`}
-		// 					fill='none'
-		// 					viewBox='0 0 24 24'
-		// 					stroke='currentColor'
-		// 				>
-		// 					<path
-		// 						strokeLinecap='round'
-		// 						strokeLinejoin='round'
-		// 						strokeWidth={2}
-		// 						d='M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z'
-		// 					/>
-		// 				</svg>
-		// 			</Link>
-
-		// 			{state ? selector?.user?.username : 'login'}
-		// 		</div>
-		// 	</div>
-		// </nav>
+		<header id='navbar'>
+			<Container>
+				<Row>
+					<Col lg={2}>
+						<div className='header-logo'>
+							<Link to='/'>
+								<img src={logo} alt='' />
+							</Link>
+						</div>
+					</Col>
+					<Col lg={8}>
+						<ul className='navbar-nav'>
+							<li className='nav-item  '>
+								<Link to='/' className='nav-link nav-active'>
+									Homepage
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Dropdown>
+									<Dropdown.Toggle className='nav-link toggle-btn'>
+										Categories
+									</Dropdown.Toggle>
+									<Dropdown.Menu>
+										<Dropdown.Item className='dropdown-item'>
+											caca
+										</Dropdown.Item>
+										<Dropdown.Item className='dropdown-item'>
+											caca
+										</Dropdown.Item>
+										<Dropdown.Item className='dropdown-item'>
+											caca
+										</Dropdown.Item>
+									</Dropdown.Menu>
+								</Dropdown>
+							</li>
+							<li className='nav-item'>
+								<Link to='/' className='nav-link'>
+									Our Blog
+								</Link>
+							</li>
+							<li className='nav-item'>
+								<Link to='/' className='nav-link'>
+									Contacts
+								</Link>
+							</li>
+						</ul>
+					</Col>
+					<Col lg={2}>
+						<div className='header-right'>
+							<Link to='/' id='search'>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									className='h-6 w-6'
+									fill='none'
+									viewBox='0 0 24 24'
+									stroke='currentColor'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth={2}
+										d='M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z'
+									/>
+								</svg>
+							</Link>
+							<Link to='/'>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									className='h-6 w-6'
+									viewBox='0 0 24 24'
+									fill='currentColor'
+								>
+									<path
+										strokeLinecap='round'
+										strokeLinejoin='round'
+										strokeWidth={2}
+										d='M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z'
+									/>
+								</svg>
+							</Link>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+		</header>
 	)
 }
 
-export default Navbar
+export default CustomNavbar
