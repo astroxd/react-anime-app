0b705571baf0db6bb8d3f1e23dad5505a98aa3fc
Andrea Sciortino <andreasciortino2004@gmail.com>
2021-10-24 02:00:59 +0200

    add characters design, fix tags

--- client/src/pages/AnimeDetails.jsx
+++ client/src/pages/AnimeDetails.jsx
@@ -227,6 +227,13 @@ const AnimeDetails = () => {
 								<div className='tag'>Piracy</div>
 								<div className='tag'>Piracy</div>
 								<div className='tag'>Piracy</div>
+								<div className='tag'>Piracy</div>
+								<div className='tag'>Piracy</div>
+								<div className='tag'>Piracy</div>
+								<div className='tag'>Piracy</div>
+								<div className='tag'>Piracy</div>
+								<div className='tag'>Piracy</div>
+								<div className='tag'>Piracy</div>
 							</div>
 						</div>
 						<div className='anime-details-character'>
@@ -246,7 +253,67 @@ const AnimeDetails = () => {
 								</Col>
 							</Row>
 							<div className='characters'>
-								<div className='character-card'></div>
+								<div className='character-card'>
+									<Row>
+										<Col xs={6} style={{ paddingRight: '0' }}>
+											<Link to='/' className='character-card-link'>
+												<div className='character-card-left'>
+													<img
+														src=' https://s4.anilist.co/file/anilistcdn/character/large/b40-q0YepZ4VUZUO.png'
+														alt=''
+													/>
+
+													<div className='description'>
+														<div className='content'>
+															<i className='fas fa-caret-right'></i>
+															<div>
+																<div className='role'>Main</div>
+																<div className='name'>Luffy</div>
+															</div>
+														</div>
+													</div>
+												</div>
+											</Link>
+										</Col>
+										<Col xs={6} style={{ paddingLeft: '0' }}>
+											<Link to='/home' className='character-card-link'>
+												<div className='character-card-right'>
+													<div className='description'>
+														<div className='content'>
+															<div>
+																<div className='name'>Luffy</div>
+																<div className='role'>Main</div>
+															</div>
+															<i className='fas fa-caret-left'></i>
+														</div>
+													</div>
+													<img
+														src=' https://s4.anilist.co/file/anilistcdn/staff/large/n95075-1qD4TeW1ON92.png'
+														alt=''
+													/>
+												</div>
+											</Link>
+										</Col>
+									</Row>
+									{/* <div className='character-card-left'>
+										<img
+											src=' https://s4.anilist.co/file/anilistcdn/character/large/b40-q0YepZ4VUZUO.png'
+											alt=''
+										/>
+										<div className='description'>
+											perohfglhsjkfshkfsjkfhsfshjkfsnaggio
+										</div>
+									</div>
+									<div className='character-card-right'>
+										<div className='description'>
+											perohfglhsjkfshkfsjkfhsfshjkfsnaggio
+										</div>
+										<img
+											src=' https://s4.anilist.co/file/anilistcdn/character/large/b40-q0YepZ4VUZUO.png'
+											alt=''
+										/>
+									</div> */}
+								</div>
 								<div className='character-card'></div>
 								<div className='character-card'></div>
 							</div>
@@ -280,7 +347,8 @@ const AnimeDetails = () => {
 			</Container>
 		</section>
 	)
-
+	// https://s4.anilist.co/file/anilistcdn/staff/large/n95075-1qD4TeW1ON92.png
+	// https://s4.anilist.co/file/anilistcdn/character/large/b40-q0YepZ4VUZUO.png
 	// return (
 	// 	<div>
 	// 		<div className='banner'>
