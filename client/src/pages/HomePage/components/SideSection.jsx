f3d68d6cbdd160978e8daef24904dc2df3d97b6e
astroxd <andreasciortino2004@gmail.com>
2021-12-24 11:15:47 +0100

    add animes.js for default animes to import, finish refactoring SideSection

--- client/src/pages/HomePage/components/SideSection.jsx
+++ client/src/pages/HomePage/components/SideSection.jsx
@@ -1,70 +1,30 @@
 import { Link } from 'react-router-dom'
+import { slides } from '../../../helpers/animes'
 
 const SideSection = () => {
-	const slides = [
-		{
-			title: 'naruto the best anime ebvah pazzurod magico super figo',
-			image:
-				'https://cdn.myanimelist.net/images/anime/1223/96541.jpg?s=faffcb677a5eacd17bf761edd78bfb3f',
-			episodes: 20,
-		},
-		{
-			title: 'naruto',
-			image:
-				'https://cdn.myanimelist.net/images/anime/1223/96541.jpg?s=faffcb677a5eacd17bf761edd78bfb3f',
-			episodes: 20,
-		},
-		{
-			title: 'naruto',
-			image:
-				'https://cdn.myanimelist.net/images/anime/1223/96541.jpg?s=faffcb677a5eacd17bf761edd78bfb3f',
-			episodes: 20,
-		},
-		{
-			title: 'naruto',
-			image:
-				'https://cdn.myanimelist.net/images/anime/1223/96541.jpg?s=faffcb677a5eacd17bf761edd78bfb3f',
-			episodes: 20,
-		},
-		{
-			title: 'naruto',
-			image:
-				'https://cdn.myanimelist.net/images/anime/1223/96541.jpg?s=faffcb677a5eacd17bf761edd78bfb3f',
-			episodes: 20,
-		},
-	]
-
 	return (
-		<div className='sidebar'>
-			<div className='sidebar-view'>
+		<div>
+			<div className='section-header'>
 				<div className='section-title'>
 					<h5>Top Views</h5>
 				</div>
-				<ul className='filter'>
-					<li className='active'>Day</li>
-					<li>Week</li>
-					<li>Month</li>
-					<li>Year</li>
-				</ul>
-				<div className='filter-gallery'>
-					{slides.map((slide, idx) => (
-						<div
-							key={idx}
-							className='side-anime-card anime-card-image'
-							style={{ backgroundImage: `url(${slide.image})` }}
-						>
-							<div className='episodes'>{`${slide.episodes} / ${slide.episodes}`}</div>
-							<div className='view'>
-								<i className='fa fa-eye' style={{ marginRight: '4px' }}></i>
-								9000
-							</div>
-							<h5>
-								<Link to='/'>{slide.title}</Link>
-							</h5>
-						</div>
-					))}
-				</div>
 			</div>
+			{slides.map((slide, idx) => (
+				<div
+					key={idx}
+					className='side-anime-card anime-card-image'
+					style={{ backgroundImage: `url(${slide.cover})` }}
+				>
+					<div className='anime-card-image-overlay episodes'>{`${slide.episodes} / ${slide.episodes}`}</div>
+					<div className='anime-card-image-overlay side-anime-card-image-overlay-view'>
+						<i className='fa fa-eye' style={{ marginRight: '4px' }}></i>
+						9000
+					</div>
+					<h5>
+						<Link to='/'>{slide.title}</Link>
+					</h5>
+				</div>
+			))}
 		</div>
 	)
 }
--- client/src/pages/HomePage/index.jsx
+++ client/src/pages/HomePage/index.jsx
@@ -4,33 +4,9 @@ import SectionTemplate from './components/SectionTemplate'
 import SideSection from './components/SideSection'
 import TrendingNow from './components/TrendingNow'
 import Carousel from '../../components/Carousel'
-import banner1 from '../../assets/images/banner1.jpg'
-import banner2 from '../../assets/images/banner2.jpg'
-import banner3 from '../../assets/images/banner3.jpg'
+import { slides } from '../../helpers/animes'
 
 const HomePage = () => {
-	const slides = [
-		{
-			title:
-				"The Story In Which I Was Kidnapped By A Young Lady's School To Be A “Sample Of The Common People”",
-			cover: banner1,
-			url: 'https://myanimelist.net/anime/1535/Death_Note',
-			rank: 1,
-		},
-		{
-			title: 'Shingeki no Kyojin',
-			cover: banner2,
-			url: 'https://myanimelist.net/anime/16498/Shingeki_no_Kyojin',
-			rank: 2,
-		},
-		{
-			title: 'Fullmetal Alchemist: Brotherhood',
-			cover: banner3,
-			url: 'https://myanimelist.net/anime/5114/Fullmetal_Alchemist__Brotherhood',
-			rank: 3,
-		},
-	]
-
 	return (
 		<div>
 			<section className='hero' style={{ overflow: 'hidden' }}>
