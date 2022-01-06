f2c73e1c4ac03ca3c70ec11dcff636a1cfe3133a
astroxd <andreasciortino2004@gmail.com>
2022-01-06 22:48:40 +0100

    add settings header ui, start settings content

--- client/src/pages/Settings/components/Profile.jsx
+++ client/src/pages/Settings/components/Profile.jsx
@@ -1,5 +1,34 @@
+import { Col, Container, Row } from 'react-bootstrap'
+
 const Profile = () => {
-	return <div>Profile page</div>
+	return (
+		<section
+			className='settings-content profile'
+			style={{ marginTop: '40px', marginBottom: '60px' }}
+		>
+			<Container>
+				<div>
+					<Row>
+						<Col>
+							<div className='section-header'>
+								<div className='section-title'>
+									<h4>Customize Profile</h4>
+								</div>
+							</div>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<div>
+								<h5>Display Name</h5>
+								<input type='text' name='' id='' />
+							</div>
+						</Col>
+					</Row>
+				</div>
+			</Container>
+		</section>
+	)
 }
 
 export default Profile
