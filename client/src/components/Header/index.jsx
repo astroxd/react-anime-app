047a4a58a7a9d9c87cc16e187ca51c116efc944b
astroxd <andreasciortino2004@gmail.com>
2022-08-28 18:20:26 +0200

    show navbar on scroll

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -7,9 +7,10 @@ import logo from '../../assets/images/logo.png'
 import NavLinks from './components/NavLinks'
 import SearchBar from './components/SearchBar'
 import UserMenu from './components/UserMenu'
-import MobileCollapseMenu from './components/MobileCollapseMenu'
-import BreadCrumbs from './components/BreadCrumbs'
 import MobileCollapseMenuButton from './components/MobileCollapseMenuButton'
+import MobileCollapseMenu from './components/MobileCollapseMenu'
+
+import BreadCrumbs from './components/BreadCrumbs'
 
 //* Style
 import './Header.css'
@@ -25,35 +26,59 @@ const Header = () => {
 		window.scrollTo(0, 0)
 	}, [pathname])
 
+	let lastPosition = 0
+	const [scoped, setScoped] = useState(false)
+	const handleScroll = () => {
+		//* If collapse menu is open navbar moves back, do not calculate scope when menu is open
+		if (showCollapseMenu) return
+		console.log('fire')
+
+		//* If lastPosition e.g 200 - current position e.g 100 is positive it means user is moving up
+		if (lastPosition - window.scrollY >= 0) setScoped(true)
+		else setScoped(false)
+
+		//* Save last position
+		lastPosition = window.scrollY
+	}
+
+	useEffect(() => {
+		document.addEventListener('scroll', handleScroll)
+		return () => {
+			document.removeEventListener('scroll', handleScroll)
+		}
+	}, [showCollapseMenu]) //* Updates with the collapse menu state
+
 	return (
-		<header id='navbar'>
-			<Container>
-				<Row xs={2}>
-					<Col lg={2}>
-						<div className='header-logo'>
-							<Link to='/'>
-								<img src={logo} alt='logo' />
-							</Link>
-						</div>
-					</Col>
-					<Col lg={8} className='header-col-middle'>
-						<NavLinks />
-					</Col>
-					<Col lg={2}>
-						<div className='header-right'>
-							<SearchBar />
-							<UserMenu />
-							<MobileCollapseMenuButton
-								showCollapseMenu={showCollapseMenu}
-								setShowCollapseMenu={setShowCollapseMenu}
-							/>
-						</div>
-					</Col>
-				</Row>
-				<MobileCollapseMenu showMenu={showCollapseMenu} />
-			</Container>
+		<>
+			<header id='navbar' className={`${scoped ? 'scoped' : ''}`}>
+				<Container>
+					<Row xs={2}>
+						<Col lg={2}>
+							<div className='header-logo'>
+								<Link to='/'>
+									<img src={logo} alt='logo' />
+								</Link>
+							</div>
+						</Col>
+						<Col lg={8} className='header-col-middle'>
+							<NavLinks />
+						</Col>
+						<Col lg={2}>
+							<div className='header-right'>
+								<SearchBar />
+								<UserMenu />
+								<MobileCollapseMenuButton
+									showCollapseMenu={showCollapseMenu}
+									setShowCollapseMenu={setShowCollapseMenu}
+								/>
+							</div>
+						</Col>
+					</Row>
+					<MobileCollapseMenu showMenu={showCollapseMenu} />
+				</Container>
+			</header>
 			<BreadCrumbs />
-		</header>
+		</>
 	)
 }
 
